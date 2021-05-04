cd7ffbb93c891d5049640bbee53f9c5310942241
Tonybonybob <tonybear.bb@gmail.com>
2021-05-04 16:01:10 +0300

    Added Agent24 project

---
+++ agent24/precache-manifest.6e5a86db7275e1e52fc484d9b020613d.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "9012598a259dbf4f1e0b",
+    "url": "/static/css/main.2753e91d.chunk.css"
+  },
+  {
+    "revision": "9012598a259dbf4f1e0b",
+    "url": "/static/js/main.9012598a.chunk.js"
+  },
+  {
+    "revision": "39797d20b78b2c0741de",
+    "url": "/static/css/1.63069677.chunk.css"
+  },
+  {
+    "revision": "39797d20b78b2c0741de",
+    "url": "/static/js/1.39797d20.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "9bc34f73a41bf43c06e833c75f586cb3",
+    "url": "/static/media/plusIcon.9bc34f73.svg"
+  },
+  {
+    "revision": "10778d6b57fa12afd6fe86de31798899",
+    "url": "/static/media/closeIcon.10778d6b.svg"
+  },
+  {
+    "revision": "4bdf738267e927edd53cd90d82cd3972",
+    "url": "/static/media/DBofObjects.4bdf7382.png"
+  },
+  {
+    "revision": "85c9691757edb7b3dfe4d3656619fcff",
+    "url": "/static/media/DBofObjects_preview.85c96917.png"
+  },
+  {
+    "revision": "67df9bc690588ce1f7baeb7223201204",
+    "url": "/static/media/playVideoIcon.67df9bc6.svg"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "da8de4b5a584a846567e75b2d71dd730",
+    "url": "/index.html"
+  }
+];
